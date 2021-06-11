5f6d01344ec7857eaaa5f38f7a91d39d3b8be317
endriazizi <endri.azizi@gmail.com>
2021-06-11 11:56:18 +0200

    azz

--- src/app/componenti/navigazione/navigazione.component.ts
+++ src/app/componenti/navigazione/navigazione.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-navigazione',
   templateUrl: './navigazione.component.html',
-  styleUrls: ['./navigazione.component.css']
+  styleUrls: ['./navigazione.component.css'],
+  providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
+
 })
 export class NavigazioneComponent implements OnInit {
 
-  constructor() { }
+
+
+  images = [
+
+    // "assets/img/clients/fem.png",
+    // "assets/img/clients/ginnastica artm.png",
+    // "assets/img/clients/ae.png",
+    {id: 1, img: "assets/img/clients/ttt.png", name:'Superman'},
+    {id: 2, img: "assets/img/clients/rit.png", name:'Batman'},
+    // {id: 5, name:'BatGirl'},
+    // {id: 3, name:'Robin'},
+    // {id: 4, name:'Flash'}
+  ];
+
+  constructor(config: NgbCarouselConfig) {
+    // 
+    config.interval = 2000;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+  }
 
   ngOnInit(): void {
   }
